8645faf2e4655b3dcd3b955d64248b6a961a5a6b
Karolkzsp5 <kondrackikarol688@gmail.com>
2026-08-05 09:06:10 +0200

    Optimization of the previous registration test

--- frontend/cypress/e2e/auth/register.cy.ts
+++ frontend/cypress/e2e/auth/register.cy.ts
@@ -1,4 +1,4 @@
-describe('Proces Rejestracji Hodowcy', () => {
+describe('Breeder Registration Process', () => {
     beforeEach(() => {
         cy.visit('/register');
     });
@@ -6,42 +6,42 @@ describe('Proces Rejestracji Hodowcy', () => {
     it('Should display a registration form with the relevant fields', () => {
         cy.get('h1').should('contain.text', 'Rejestracja Hodowcy PZHGP');
 
-        cy.get('label').contains('Imię').should("exists").and('be.visible');
-        cy.get('input[name="name"]').should('exist').and('be.visible');
+        cy.contains('label', 'Imię').should('be.visible');
+        cy.get('input[name="name"]').should('be.visible');
 
-        cy.get('label').contains('Nazwisko').should("exists").and('be.visible');
-        cy.get('input[name="surname"]').should('exist').and('be.visible');
+        cy.contains('label', 'Nazwisko').should('be.visible');
+        cy.get('input[name="surname"]').should('be.visible');
 
-        cy.get('label').contains('Data urodzenia').should("exists").and('be.visible');
-        cy.get('input[name="dateOfBirth"]').should('exist').and('be.visible');
+        cy.contains('label', 'Data urodzenia').should('be.visible');
+        cy.get('input[name="dateOfBirth"]').should('be.visible');
 
-        cy.get('label').contains('Adres E-mail').should("exists").and('be.visible');
-        cy.get('input[name="email"]').should('exist').and('be.visible');
+        cy.contains('label', 'Adres E-mail').should('be.visible');
+        cy.get('input[name="email"]').should('be.visible');
 
-        cy.get('label').contains('Numer telefonu').should("exists").and('be.visible');
-        cy.get('input[name="phoneNumber"]').should('exist').and('be.visible');
+        cy.contains('label', 'Numer telefonu').should('be.visible');
+        cy.get('input[name="phoneNumber"]').should('be.visible');
 
-        cy.get('label').contains('Hasło').should("exists").and('be.visible');
-        cy.get('input[name="password"]').should('exist').and('be.visible');
+        cy.contains('label', 'Hasło').should('be.visible');
+        cy.get('input[name="password"]').should('be.visible');
 
-        cy.get('label').contains('Powtórz hasło').should("exists").and('be.visible');
-        cy.get('input[name="confirmPassword"]').should('exist').and('be.visible');
+        cy.contains('label', 'Powtórz hasło').should('be.visible');
+        cy.get('input[name="confirmPassword"]').should('be.visible');
 
-        cy.get('label').contains('Wybierz sekcję do której chcesz należeć').should("exists").and('be.visible');
-        cy.get('select[name="sectionId"]').should('exist').and('be.visible');
+        cy.contains('label', 'Wybierz sekcję do której chcesz należeć').should('be.visible');
+        cy.get('select[name="sectionId"]').should('be.visible');
 
-        cy.get('label').contains('Kod pocztowy').should("exists").and('be.visible');
-        cy.get('input[name="postalCode"]').should('exist').and('be.visible');
+        cy.contains('label', 'Kod pocztowy').should('be.visible');
+        cy.get('input[name="postalCode"]').should('be.visible');
 
-        cy.get('label').contains('Miejscowość').should("exists").and('be.visible');
-        cy.get('input[name="city"]').should('exist').and('be.visible');
+        cy.contains('label', 'Miejscowość').should('be.visible');
+        cy.get('input[name="city"]').should('be.visible');
 
-        cy.get('label').contains('Ulica').should("exists").and('be.visible');
-        cy.get('input[name="street"]').should('exist').and('be.visible');
+        cy.contains('label', 'Ulica').should('be.visible');
+        cy.get('input[name="street"]').should('be.visible');
 
         cy.get('button[type="submit"]').should('be.visible').and('not.be.disabled');
 
-        cy.contains('Masz już konto?').should("exists").and('be.visible');
-        cy.get('a').should('contain.text', 'Zaloguj się tutaj').and("exists").and('be.visible');
+        cy.contains('Masz już konto?').should('be.visible');
+        cy.get('a').should('contain.text', 'Zaloguj się tutaj').and('be.visible');
     });
 });
